fd941d26f371395efd629530b675d47feb1b14f9
Dounya ALAOUI <dounyaa.alaoui@gmail.com>
2026-08-24 15:39:21 +0200

    fix: handle missing DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -1,10 +1,12 @@
-from sqlalchemy import create_engine, StaticPool
+from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL is None:
+    raise RuntimeError("DATABASE_URL is missing. Please verify your .env file.")
 
 engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(bind = engine, autoflush=False, expire_on_commit = False)
