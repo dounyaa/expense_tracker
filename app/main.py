72bf68e03b59eee4ddfe0009da122702e2695365
Dounya ALAOUI <dounyaa.alaoui@gmail.com>
2026-08-25 21:51:50 +0200

    feat: configure CORS for frontend access

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.routers.expenses import router as expenses_router
 from app.database import Base, engine
 from app import models
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +14,17 @@ async def lifespan(app: FastAPI):
     engine.dispose()
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+    "http://127.0.0.1:5500",
+],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(expenses_router)
 
 @app.get("/health")
